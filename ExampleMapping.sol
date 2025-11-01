26c1cabe4c12ff2e28f4b5fc8860d4039f87a1b4
Kashika Singhal <146356598+kashika24@users.noreply.github.com>
2025-11-01 16:49:20 +0530

    Update ExampleMapping.sol

--- ExampleMapping.sol
+++ ExampleMapping.sol
@@ -12,6 +12,13 @@ contract ExampleMapping{
         return address(this).balance;
     }
 
+    //to transfer only limited balance, less than or equal to what is available
+     function withdrawAmount(address payable _to, uint _amount)public {
+        require(_amount<=balanceRecieved[msg.sender], "not enough funds!");
+        balanceRecieved[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
+
     function withdrawAll(address payable _to) public {
         uint depositedvalue = balanceRecieved[msg.sender];
         balanceRecieved[msg.sender] = 0;
